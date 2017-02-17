70a09299d8aa58d00e6d9121e9d00a8ae9c0243f
swcraig <sebastian.w.craig@gmail.com>
2017-02-17 12:26:52 -0800

    Parse json in posts to Braminus

--- app/braminus.rb
+++ app/braminus.rb
@@ -6,6 +6,8 @@ require './app/a_star'
 
 # The real deal BattleSnake 2017
 class Braminus < Sinatra::Base
+  SNAKE_NAME = 'Braminus'.freeze
+  COLOUR = '#8B008B'.freeze
   UP = 'up'.freeze
   DOWN = 'down'.freeze
   LEFT = 'left'.freeze
@@ -16,13 +18,31 @@ class Braminus < Sinatra::Base
   end
 
   post '/start' do
-    @game_id = params[:game_id]
-    @grid = Grid.new(params[:width], params[:height])
-    { name: 'Braminus', color: '#8B008B' }.to_json
+    params = parse_post(request.body.read)
+    @game_id = params['game_id']
+    @grid = Grid.new(params['width'], params['height'])
+    { name: SNAKE_NAME, color: COLOUR }.to_json
   end
 
   post '/move' do
+    params = parse_post(request.body.read)
+    food = params['food'].first
+    AStar.new([0, 0], food, @grid, find_obstacles(params['snake'])).search
     { move: UP }.to_json
+  end
+
+  private
+
+  def parse_post(request)
+    JSON.parse(request)
+  end
+
+  def find_obstacles(snakes)
+    all_occupied = []
+    snakes.each do |s|
+      all_occupied.push(s.coords)
+    end
+    all_occupied.flatten
   end
 
   run! if app_file == $PROGRAM_NAME
