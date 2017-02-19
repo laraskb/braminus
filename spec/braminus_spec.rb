a03ae3247f3adaf263cc8897524ac5c7fab7c422
swcraig <sebastian.w.craig@gmail.com>
2017-02-19 09:10:46 -0800

    Ensure that Braminus responds quickly enough

--- spec/braminus_spec.rb
+++ spec/braminus_spec.rb
@@ -1,3 +1,4 @@
+require 'benchmark'
 require 'spec_helper'
 require 'rack/test'
 require './app/braminus'
@@ -12,21 +13,23 @@ RSpec.describe Braminus do
 
   it 'responds to post at /start' do
     expected = { name: 'Braminus', color: '#8B008B' }.to_json
-    post '/start', { width: 20, height: 20, game_id: 'b1d-a112-4e0e' }.to_json
+    post_data = { width: 20, height: 20, game_id: 'b1d-a112-4e0e' }.to_json
+    within_limit(post('/start', post_data))
     expect(last_response.body).to eq(expected)
   end
 
   it 'responds to post at /move' do
     expected = { move: 'right' }.to_json
     post '/start', { width: 20, height: 20, game_id: 'b1d-a112-4e0e' }.to_json
-    post '/move', fixture('./spec/fixtures/move_parameters.json')
+    within_limit(post('/move', fixture('./spec/fixtures/move_parameters.json')))
     expect(last_response.body).to eq(expected)
   end
 
   it 'moves towards tail when a different snake is closer to food' do
     expected = { move: 'right' }.to_json
     post '/start', { width: 20, height: 20, game_id: 'b1d-a112-4e0e' }.to_json
-    post '/move', fixture('./spec/fixtures/move_parameters_others.json')
+    filename = './spec/fixtures/move_parameters_others.json'
+    within_limit(post('/move', fixture(filename)))
     expect(last_response.body).to eq(expected)
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,14 @@
+require 'benchmark'
+
+LIMIT = 0.2 # Braminus needs to respond within 200 milliseconds
+
+def within_limit(endpoint_call)
+  t = Benchmark.realtime do
+    endpoint_call
+  end
+  expect(t).to be < LIMIT
+end
+
 def fixture(file)
   File.read(File.expand_path(file))
 end
