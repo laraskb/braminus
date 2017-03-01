40b7b1d08267796ef7199009829972a05ab7f0e5
tzinquaw <tzinquaw.bp@gmail.com>
2017-02-28 23:13:28 -0800

    removed unneccessary posts, the info was already being sent in the json file

--- spec/braminus_spec.rb
+++ spec/braminus_spec.rb
@@ -20,14 +20,12 @@ RSpec.describe 'Braminus' do
 
   it 'responds to post at /move' do
     expected = { move: 'right' }.to_json
-    post '/start', { width: 4, height: 4, game_id: 'b1d-a112-4e0e' }.to_json
     within_limit(post('/move', fixture('./spec/fixtures/move_parameters.json')))
     expect(last_response.body).to eq(expected)
   end
 
   it 'moves towards tail when a different snake is closer to food' do
     expected = { move: 'right' }.to_json
-    post '/start', { width: 20, height: 20, game_id: 'b1d-a112-4e0e' }.to_json
     filename = './spec/fixtures/move_parameters_others.json'
     within_limit(post('/move', fixture(filename)))
     expect(last_response.body).to eq(expected)
@@ -35,7 +33,6 @@ RSpec.describe 'Braminus' do
 
   it 'moves towards tail when a longer snake could cut us off' do
     expected = { move: 'left' }.to_json
-    post '/start', { width: 4, height: 5, game_id: 'b1d-a112-4e0e' }.to_json
     filename = './spec/fixtures/move_longer_snake_head.json'
     within_limit(post('/move', fixture(filename)))
     expect(last_response.body).to eq(expected)
@@ -43,7 +40,6 @@ RSpec.describe 'Braminus' do
 
   it 'will not go at food if we would enter a dead-end' do
     expected = { move: 'down' }.to_json
-    post '/start', { width: 4, height: 4, game_id: 'b1d-a112-4e0e' }.to_json
     filename = './spec/fixtures/no_escape.json'
     within_limit(post('/move', fixture(filename)))
     expect(last_response.body).to eq(expected)
@@ -51,7 +47,6 @@ RSpec.describe 'Braminus' do
 
   it 'chooses the closest food' do
     expected = { move: 'down' }.to_json
-    post '/start', { width: 4, height: 4, game_id: 'b1d-a112-4e0e' }.to_json
     filename = './spec/fixtures/closest_food.json'
     within_limit(post('/move', fixture(filename)))
     expect(last_response.body).to eq(expected)
@@ -59,7 +54,6 @@ RSpec.describe 'Braminus' do
 
   it 'updates the dead_space on tail checks' do
     expected = { move: 'down' }.to_json
-    post '/start', { width: 4, height: 5, game_id: 'b1d-a112-4e0e' }.to_json
     filename = './spec/fixtures/updated_deadspace.json'
     within_limit(post('/move', fixture(filename)))
     expect(last_response.body).to eq(expected)
@@ -67,7 +61,6 @@ RSpec.describe 'Braminus' do
 
   it 'will move to an open space if it is trapped' do
     expected = { move: 'down' }.to_json
-    post '/start', { width: 4, height: 4, game_id: 'b1d-a112-4e0e' }.to_json
     filename = './spec/fixtures/trapped.json'
     within_limit(post('/move', fixture(filename)))
     expect(last_response.body).to eq(expected)
@@ -75,7 +68,6 @@ RSpec.describe 'Braminus' do
 
   it 'will move to an open space if it is trapped in a total way' do
     expected = { move: 'up' }.to_json
-    post '/start', { width: 4, height: 4, game_id: 'b1d-a112-4e0e' }.to_json
     filename = './spec/fixtures/trapped_again.json'
     within_limit(post('/move', fixture(filename)))
     expect(last_response.body).to eq(expected)
@@ -83,7 +75,6 @@ RSpec.describe 'Braminus' do
 
   it 'knows that another snakes tail is not dead space' do
     expected = { move: 'right' }.to_json
-    post '/start', { width: 4, height: 4, game_id: 'b1d-a112-4e0e' }.to_json
     filename = './spec/fixtures/other_snake_tails.json'
     within_limit(post('/move', fixture(filename)))
     expect(last_response.body).to eq(expected)
